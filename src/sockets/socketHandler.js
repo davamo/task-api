a045b068caa668f6a1bd2063961907a760189db7
davamo.cl <davamo@davamo.cl>
2025-05-26 16:45:18 -0400

    feat: create socketHandler module and handle client connection

---
+++ src/sockets/socketHandler.js
@@ -0,0 +1,9 @@
+module.exports = (io) => {
+  io.on("connection", (socket) => {
+    console.log("🔌 Cliente conectado:", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("❌ Cliente desconectado:", socket.id);
+    });
+  });
+};
